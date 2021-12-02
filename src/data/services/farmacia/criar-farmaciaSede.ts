74e8d0177c028b08743d1bb8bf47bad03280e98b
“Breno <brenorodriguesdev@gmail.com>
2021-12-02 10:35:03 -0300

    feat: add criar farmacia sede service

---
+++ src/data/services/farmacia/criar-farmaciaSede.ts
@@ -0,0 +1,10 @@
+import { FarmaciaSedeModel } from "../../../domain/models/farmacia/farmaciaSede";
+import { CriarFarmaciaSedeUseCase } from "../../../domain/useCases/farmacia/criar-farmaciaSede";
+import { GrpcClient } from "../../contracts/grpc-client";
+
+export class CriarFarmaciaFilialService implements CriarFarmaciaSedeUseCase {
+    constructor (private readonly grpcClient: GrpcClient) {}
+    async criar(data: FarmaciaSedeModel): Promise<void | Error> {
+        await this.grpcClient.call('farmacia.proto',process.env.GRPC_CONNECTION_FARMACIA, 'FarmaciaService', 'criarSede', data)
+    }
+}
