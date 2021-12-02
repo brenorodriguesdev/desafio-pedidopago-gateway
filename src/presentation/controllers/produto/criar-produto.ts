544f8d6e5909123a892010ad398d7e3ffc031e89
“Breno <brenorodriguesdev@gmail.com>
2021-12-02 10:07:27 -0300

    feat: add criar produto controller

---
+++ src/presentation/controllers/produto/criar-produto.ts
@@ -0,0 +1,41 @@
+import { CriarProdutoUseCase } from "../../../domain/useCases/produto/criar-produtos"
+import { Validator } from "../../../validation/contracts/validator"
+import { Controller } from "../../contracts/controller"
+import { HttpRequest, HttpResponse } from "../../contracts/http"
+import { badRequest, ok } from "../../contracts/http-helper"
+
+export class ClonarProdutoController implements Controller {
+    constructor(private readonly validator: Validator, private readonly criarProdutoUseCase: CriarProdutoUseCase) { }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const error = this.validator.validate(httpRequest.body)
+            if (error) {
+                return badRequest(error)
+            }
+
+            const {
+                thumbnail,
+                nome,
+                preco,
+                ingredientes,
+                disponibilidade,
+                volume,
+                outros
+            } = httpRequest.body
+
+            const produto = await this.criarProdutoUseCase.criar({
+                thumbnail,
+                nome,
+                preco,
+                ingredientes,
+                disponibilidade,
+                volume,
+                outros
+            })
+
+            return ok(produto)
+        } catch (error) {
+            return badRequest(error)
+        }
+    }
+}
