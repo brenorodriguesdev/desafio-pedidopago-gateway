c5a5473651b29e5ab213ac975d513451ab194248
“Breno <brenorodriguesdev@gmail.com>
2021-12-02 19:37:18 -0300

    test: deletarFarmaciaFilial service

---
+++ src/data/services/farmacia/deletar-farmaciaFilial.spec.ts
@@ -0,0 +1,41 @@
+import { GrpcClient } from "../../contracts/grpc-client"
+import { DeletarFarmaciaFilialService } from "./deletar-farmaciaFilial"
+
+interface SutTypes {
+    grpcClient: GrpcClient
+    sut: DeletarFarmaciaFilialService
+}
+
+const makeGrpcClient = (): GrpcClient => {
+    class GrpcClientStub implements GrpcClient {
+        call(proto: string, connection: string, service: string, func: string, payload?: any, metadata?: any): Promise<any> {
+            return new Promise(resolve => resolve(null))
+        }
+    }
+    return new GrpcClientStub()
+}
+
+const makeSut = (): SutTypes => {
+    const grpcClient = makeGrpcClient()
+    const sut = new DeletarFarmaciaFilialService(grpcClient)
+    return {
+        grpcClient,
+        sut
+    }
+}
+
+describe('DeletarFarmaciaFilial Service', () => {
+    test('Garantir que call seja chamado com os valores corretos', async () => {
+        const { sut, grpcClient } = makeSut()
+        const callSpy = jest.spyOn(grpcClient, 'call')
+        await sut.deletar(1)
+        expect(callSpy).toHaveBeenCalledWith('farmacia.proto', process.env.GRPC_CONNECTION_FARMACIA, 'FarmaciaService', 'deletarFilial', { id: 1 })
+    })
+
+    test('Garantir que se o call retornar uma exceção o serviço repassará a exceção', async () => {
+        const { sut, grpcClient } = makeSut()
+        jest.spyOn(grpcClient, 'call').mockImplementationOnce(() => { throw new Error() })
+        const promise = sut.deletar(1)
+        await expect(promise).rejects.toThrow()
+      })
+})
